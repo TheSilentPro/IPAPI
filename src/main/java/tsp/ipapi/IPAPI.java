4f7bdd1c53309798a452b8aed094f20f97be6a41
Silent <perovskiboki@mail.com>
2021-06-28 17:46:38 +0200

    Remove example from main

--- src/main/java/tsp/ipapi/IPAPI.java
+++ src/main/java/tsp/ipapi/IPAPI.java
@@ -28,28 +28,4 @@ public final class IPAPI {
         return new BatchIPRequester(ips).execute();
     }
 
-    public static void main(String[] args) throws IOException, ParseException {
-        IPResponse ipResponse = new IPRequester("8.8.8.8")
-                .withCurrency()
-                .withZip()
-                .execute();
-        BatchIPResponse batchIPResponse = new BatchIPRequester()
-                .addIP("8.8.4.4", Field.CITY, Field.CONTINENT)
-                .addIP("24.48.0.1", Field.STATUS, Field.COUNTRY)
-                .execute();
-
-        System.out.println(" > Single IP");
-        for (Map.Entry<Field, String> entry : ipResponse.getFields().entrySet()) {
-            System.out.println(entry.getKey().getName() + ": " + entry.getValue());
-        }
-
-        System.out.println("\n > Batch IPs");
-        for (Map.Entry<String, Map<Field, String>> ip : batchIPResponse.getResponses().entrySet()) {
-            Map<Field, String> entries = batchIPResponse.getFields(ip.getKey());
-            System.out.println(" IP: " + ip.getKey());
-            entries.forEach((field, value) -> System.out.println(field.getName() +  ": " + value));
-            System.out.println(" ");
-        }
-    }
-
 }
